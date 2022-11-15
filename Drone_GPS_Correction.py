8e77e4c6cf150601dfb93c58ddb2146ccb35a04a
ariyanzarei@email.arizona.edu <ariyanzarei@email.arizona.edu>
2022-11-14 22:02:48 -0700

    updating code for readability and reusability

--- Drone_GPS_Correction.py
+++ Drone_GPS_Correction.py
@@ -5,225 +5,267 @@ import sys
 import os
 import json
 import computer_vision_utils as cv_util
+import argparse
 
-def report_time(start,end):
-	print('-----------------------------------------------------------')
-	print('Start date time: {0}\nEnd date time: {1}\nTotal running time: {2}.'.format(start,end,end-start))
+
+def report_time(start, end):
+    print("-----------------------------------------------------------")
+    print(
+        "Start date time: {0}\nEnd date time: {1}\nTotal running time: {2}.".format(
+            start, end, end - start
+        )
+    )
+
 
 def get_anchors_from_json(path):
-	
-	with open(path, "r") as outfile:  
-		anchors_dict = json.load(outfile)
 
-	return anchors_dict
+    with open(path, "r") as outfile:
+        anchors_dict = json.load(outfile)
 
-def save_json_results(json_path,res,experiment_Id):
+    return anchors_dict
 
-	with open(json_path, "r") as outfile:  
-		current_results = json.load(outfile)
 
-	if General_GPS_Correction.settings.do_cross_validation:
+def save_json_results(json_path, res, experiment_Id):
 
-		current_results[experiment_Id]={"GCP_RMSE":res[0],"Proj_RMSE":res[1],"Proj_RMSE":res[2],"Time":str(res[3]),\
-			"CV_GCP_RMSE_AVG":res[4],"CV_GCP_RMSE_STD":res[5],\
-			"CV_Proj_RMSE_AVG":res[6],"CV_Proj_RMSE_STD":res[7],\
-			"CV_Proj_RMSE_Norm_AVG":res[8],"CV_Proj_RMSE_Norm_STD":res[9],\
-			"CV_Time_AVG":str(res[10])}
-	else:
+    with open(json_path, "r") as outfile:
+        current_results = json.load(outfile)
 
-		current_results[experiment_Id]={"GCP_RMSE":res[0],"Proj_RMSE":res[1],"Proj_RMSE_Norm":res[2],"Time":str(res[3])}
+    if General_GPS_Correction.settings.do_cross_validation:
 
-	with open(json_path, 'w') as outfile:
-		json.dump(current_results, outfile)
-	
-def save_json_sim_gcps(json_path,list_gcps,dataset_name):
-	
-	with open(json_path, "r") as outfile:  
-		current_results = json.load(outfile)
+        current_results[experiment_Id] = {
+            "GCP_RMSE": res[0],
+            "Proj_RMSE": res[1],
+            "Proj_RMSE": res[2],
+            "Time": str(res[3]),
+            "CV_GCP_RMSE_AVG": res[4],
+            "CV_GCP_RMSE_STD": res[5],
+            "CV_Proj_RMSE_AVG": res[6],
+            "CV_Proj_RMSE_STD": res[7],
+            "CV_Proj_RMSE_Norm_AVG": res[8],
+            "CV_Proj_RMSE_Norm_STD": res[9],
+            "CV_Time_AVG": str(res[10]),
+        }
+    else:
 
-	current_results[dataset_name] = list_gcps
+        current_results[experiment_Id] = {
+            "GCP_RMSE": res[0],
+            "Proj_RMSE": res[1],
+            "Proj_RMSE_Norm": res[2],
+            "Time": str(res[3]),
+        }
 
-	with open(json_path, 'w') as outfile:
-		json.dump(current_results, outfile)
+    with open(json_path, "w") as outfile:
+        json.dump(current_results, outfile)
 
-def load_json_sim_gcps(json_path,dataset_name):
-	
-	with open(json_path, "r") as outfile:  
-		current_results = json.load(outfile)
-	
-	return current_results[dataset_name]
 
-def modify_settings():
-	General_GPS_Correction.settings.grid_w = 3
-	General_GPS_Correction.settings.grid_h = 7
-	General_GPS_Correction.settings.scale = 0.2
-	General_GPS_Correction.settings.nearest_number = 4
-	General_GPS_Correction.settings.discard_transformation_perc_inlier = 0.4
-	General_GPS_Correction.settings.transformation = cv_util.Transformation.similarity
-	General_GPS_Correction.settings.percentage_next_neighbor = 0.6
-	General_GPS_Correction.settings.cores_to_use = 55
-	General_GPS_Correction.settings.draw_GCPs = True
-	General_GPS_Correction.settings.max_no_inliers = 5
-	General_GPS_Correction.settings.number_bins = 5
-	General_GPS_Correction.settings.size_bins = 5
-	General_GPS_Correction.settings.do_cross_validation = False
-	General_GPS_Correction.settings.sub_set_choosing = False
-	General_GPS_Correction.settings.N_perc = 0.5
-	General_GPS_Correction.settings.E_perc = 0.5
+def save_json_sim_gcps(json_path, list_gcps, dataset_name):
+
+    with open(json_path, "r") as outfile:
+        current_results = json.load(outfile)
+
+    current_results[dataset_name] = list_gcps
+
+    with open(json_path, "w") as outfile:
+        json.dump(current_results, outfile)
+
+
+def load_json_sim_gcps(json_path, dataset_name):
+
+    with open(json_path, "r") as outfile:
+        current_results = json.load(outfile)
+
+    return current_results[dataset_name]
+
+
+def load_settings(args.settings):
+    with open(args.settings, "r") as f:
+        settings_dict = json.load(f)
+
+    General_GPS_Correction.settings.grid_w = settings_dict["grid_w"]
+    General_GPS_Correction.settings.grid_h = settings_dict["grid_h"]
+    General_GPS_Correction.settings.scale = settings_dict["scale"]
+    General_GPS_Correction.settings.nearest_number = settings_dict["nearest_number"]
+    General_GPS_Correction.settings.discard_transformation_perc_inlier = settings_dict[
+        "discard_transformation_perc_inlier"
+    ]
+    General_GPS_Correction.settings.transformation = getattr(
+        cv_util.Transformation, settings_dict["transformation"]
+    )
+    General_GPS_Correction.settings.percentage_next_neighbor = settings_dict[
+        "percentage_next_neighbor"
+    ]
+    General_GPS_Correction.settings.cores_to_use = settings_dict["cores_to_use"]
+    General_GPS_Correction.settings.draw_GCPs = settings_dict["draw_GCPs"]
+    General_GPS_Correction.settings.max_no_inliers = settings_dict["max_no_inliers"]
+    General_GPS_Correction.settings.number_bins = settings_dict["number_bins"]
+    General_GPS_Correction.settings.size_bins = settings_dict["size_bins"]
+    General_GPS_Correction.settings.do_cross_validation = settings_dict[
+        "do_cross_validation"
+    ]
+    General_GPS_Correction.settings.sub_set_choosing = settings_dict["sub_set_choosing"]
+    General_GPS_Correction.settings.N_perc = settings_dict["N_perc"]
+    General_GPS_Correction.settings.E_perc = settings_dict["E_perc"]
+
+
+def get_args():
+    parser = argparse.ArgumentParser(
+        description="MegaStitch Drone Stitching and Geo-correction script.",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+    )
+
+    parser.add_argument(
+        "-d",
+        "--data",
+        help="The path to the data directory.",
+        metavar="data",
+        required=True,
+        type=str,
+    )
+
+    parser.add_argument(
+        "-r",
+        "--result",
+        help="The path to the directory where the results will be saved.",
+        metavar="result",
+        required=True,
+        type=str,
+    )
+
+    parser.add_argument(
+        "-g",
+        "--gcp",
+        help="The path to Ground Control Points (GCPs) files. Refer to readme for formatting of the json/csv file.",
+        metavar="gcp",
+        required=False,
+        type=str,
+    )
+
+    parser.add_argument(
+        "-s",
+        "--settings",
+        help="The path to the json file that contains the configuration/settings information.",
+        metavar="settings",
+        required=True,
+        type=str,
+    )
+
+    return parser.parse_args()
+
 
 def main():
-	
-	if len(sys.argv)==6:
-		data_path = sys.argv[1]
-		result_path = sys.argv[2]
-		sift_dir = sys.argv[3]
-		anchor_path = sys.argv[4]
-		experiment_Id = sys.argv[5]
 
-		main_result_path = result_path
+    args = get_args()
 
-		result_path = '{0}/{1}'.format(result_path,experiment_Id)
+    if not os.path.exists(args.result):
+        os.makedirs(args.result)
 
-		if not os.path.exists(result_path):
-			os.mkdir(result_path)
+    ortho_path = os.path.join(args.result, "ortho.png")
+    plot_path = os.path.join(args.result, "initial_GPS.png")
+    corrected_coordinates_path = plot_path = os.path.join(
+        args.result, "corrected_coordinates.json"
+    )
+    log_path = os.path.join(args.result, "log.txt")
+    sift_path = os.path.join(args.result, "SIFT")
 
-		method = experiment_Id.split('_')[0]
-		dataset_name = experiment_Id.split('_')[1]
+    if not os.path.exists(sift_path):
+        os.makedirs(sift_path)
 
-		data_set_path = '{0}/IMDS_{1}'.format(data_path,dataset_name)
-		ortho_path = '{0}/{1}_ortho.png'.format(result_path,experiment_Id)
-		plot_path = '{0}/{1}_initial_GPS.png'.format(result_path,experiment_Id)
-		corrected_coordinates_path = '{0}/{1}_corrected_coordinates.json'.format(result_path,experiment_Id)
-		log_path = '{0}/{1}_log.txt'.format(result_path,experiment_Id)
-		sift_path = '{0}/{1}'.format(sift_dir,dataset_name[:3])
-		json_path = '{0}/results.json'.format(main_result_path)
-		sim_GCPs_path = '{0}/sim_GCPs.json'.format(main_result_path)
-		tr_path = '{0}/pairwise_transformations.json'.format(result_path)
+    General_GPS_Correction.init_setting(args.data)
+    General_GPS_Correction.settings.Dataset = os.path.basename(
+        os.path.normpath(args.data)
+    )
+    General_GPS_Correction.settings.args.method = args.method
+    General_GPS_Correction.settings.AllGCPRMSE = True
 
-		if not os.path.exists(sift_path):
-			os.mkdir(sift_path)
+    load_settings(args.settings)
 
-		General_GPS_Correction.init_setting(data_set_path)
+    original = sys.stdout
+    log_file = open(log_path, "w")
+    sys.stdout = log_file
 
-		General_GPS_Correction.settings.Dataset = dataset_name
-		General_GPS_Correction.settings.Method = method
-		General_GPS_Correction.settings.AllGCPRMSE = True
-		
-		modify_settings()
+    start_time = datetime.datetime.now()
 
-	else:
-		data_path = '/home/ariyan/Desktop/Drone/small'
-		result_path = '/home/ariyan/Desktop/Drone/test'
-		experiment_Id = 'EXP_local'
-		sift_dir = '/home/ariyan/Desktop/Drone/SIFT'
+    if hasattr(args,"gcp"):
+        anchors_dict = get_anchors_from_json(args.gcp)
+    else:
+        anchors_dict = None
 
-		result_path = '{0}/{1}'.format(result_path,experiment_Id)
+    field = General_GPS_Correction.Field(sift_p=sift_path)
 
-		if not os.path.exists(result_path):
-			os.mkdir(result_path)
+    if General_GPS_Correction.settings.transformation == cv_util.Transformation.similarity:
+        
+        (
+            coords_dict,
+            H,
+            H_inv,
+            abs_tr,
+            res,
+            Sim_GCPs,
+            all_GCPs,
+        ) = field.geo_correct_MegaStitchSimilarity(anchors_dict)
+    elif General_GPS_Correction.settings.transformation == cv_util.Transformation.affine:
+        (
+            coords_dict,
+            H,
+            H_inv,
+            abs_tr,
+            res,
+            all_GCPs,
+        ) = field.geo_correct_MegaStitchAffine(anchors_dict, Sim_GCPs)
+    elif General_GPS_Correction.settings.transformation == cv_util.Transformation.homography:
+        if General_GPS_Correction.settings.preprocessing_transformation == "None":
+            (
+                coords_dict,
+                H,
+                H_inv,
+                abs_tr,
+                res,
+                all_GCPs,
+            ) = field.geo_correct_BundleAdjustment_Homography(anchors_dict, Sim_GCPs)
+        elif General_GPS_Correction.settings.preprocessing_transformation == "similarity":
+            (
+                coords_dict,
+                H,
+                H_inv,
+                abs_tr,
+                res,
+                all_GCPs,
+            ) = field.geo_correct_MegaStitch_Similarity_Bundle_Adjustment_Homography(
+                anchors_dict, Sim_GCPs
+            )
+        elif General_GPS_Correction.settings.preprocessing_transformation == "affine":
+            (
+                coords_dict,
+                H,
+                H_inv,
+                abs_tr,
+                res,
+                all_GCPs,
+            ) = field.geo_correct_MegaStitch_Affine_Bundle_Adjustment_Homography(
+                anchors_dict, Sim_GCPs
+            )
+   
+    if H is None:
+        gcp_inf = None
+    else:
+        gcp_inf = (anchors_dict, H_inv, abs_tr)
 
-		method = 'BNDL'
-		 
-		data_set_path = data_path
-		ortho_path = '{0}/{1}_ortho.png'.format(result_path,experiment_Id)
-		plot_path = '{0}/{1}_initial_GPS.png'.format(result_path,experiment_Id)
-		corrected_coordinates_path = '{0}/{1}_corrected_coordinates.json'.format(result_path,experiment_Id)
-		log_path = '{0}/{1}_log.txt'.format(result_path,experiment_Id)
-		sift_path = '{0}'.format(sift_dir)
+    field.generate_transformation_accuracy_histogram(
+        coords_dict, plot_path.replace("initial_GPS", "transformation_plot")
+    )
+    field.save_field_centers_visualization(plot_path)
 
-		General_GPS_Correction.init_setting(data_set_path)
-		modify_settings()
-		
-		General_GPS_Correction.settings.cores_to_use=2
-		General_GPS_Correction.settings.transformation = cv_util.Transformation.translation
-		General_GPS_Correction.settings.discard_transformation_perc_inlier = 0.0
+    ortho = field.generate_field_ortho(coords_dict, gcp_info=gcp_inf)
 
-	original = sys.stdout
-	log_file = open(log_path, 'w')
-	sys.stdout = log_file
+    field.save_field_ortho(ortho, ortho_path)
+    field.save_field_coordinates(corrected_coordinates_path, coords_dict)
 
-	start_time = datetime.datetime.now()
+    end_time = datetime.datetime.now()
 
-	anchors_dict = get_anchors_from_json(anchor_path)
+    report_time(start_time, end_time)
 
-	field = General_GPS_Correction.Field(sift_p=sift_path,tr_p=tr_path)
-	H = None
-
-	if method == 'BNDL':
-		coords_dict = field.geo_correct_BundleAdjustment()
-	elif method == 'MGRAPH':
-		coords_dict = field.geo_correct_MGRAPH()
-	elif method == 'MEGA3':
-		coords_dict = field.geo_correct_MEGASTITCH()
-	elif method == 'MEGAHMG':
-		coords_dict = field.geo_correct_MEGASTITCH_Groups()
-	elif method == 'MEGA3BNDL':
-		coords_dict = field.geo_correct_MEGASTITCH()
-		coords_dict = field.geo_correct_BundleAdjustment(coords_dict)
-	elif method == 'MEGAHMGBNDL':
-		coords_dict = field.geo_correct_MEGASTITCH_Groups()
-		coords_dict = field.geo_correct_BundleAdjustment(coords_dict)
-	elif method == 'MEGAH':
-		coords_dict = field.geo_correct_MegaStitch_Homography()
-	elif method == 'MEGASTITCH-SIM':
-		General_GPS_Correction.settings.transformation = cv_util.Transformation.similarity
-		coords_dict,H,H_inv,abs_tr,res,Sim_GCPs,all_GCPs = field.geo_correct_MegaStitchSimilarity(anchors_dict)
-		if dataset_name != "ODFN":
-			save_json_sim_gcps(sim_GCPs_path,Sim_GCPs,dataset_name)
-	elif method == 'MEGASTITCH-AFF':
-		Sim_GCPs = load_json_sim_gcps(sim_GCPs_path,dataset_name)
-		General_GPS_Correction.settings.transformation = cv_util.Transformation.affine
-		coords_dict,H,H_inv,abs_tr,res,all_GCPs = field.geo_correct_MegaStitchAffine(anchors_dict,Sim_GCPs)
-	elif method == 'BNDL-ADJ':
-		if dataset_name != "ODFN":
-			Sim_GCPs = load_json_sim_gcps(sim_GCPs_path,dataset_name)
-		else:
-			Sim_GCPs=None
-		General_GPS_Correction.settings.transformation = cv_util.Transformation.homography
-		coords_dict,H,H_inv,abs_tr,res,all_GCPs = field.geo_correct_BundleAdjustment_Homography(anchors_dict,Sim_GCPs)
-	elif method == 'MEGASTITCH-AFF-BNDL-ADJ':
-		Sim_GCPs = load_json_sim_gcps(sim_GCPs_path,dataset_name)
-		coords_dict,H,H_inv,abs_tr,res,all_GCPs = field.geo_correct_MegaStitch_Affine_Bundle_Adjustment_Homography(anchors_dict,Sim_GCPs)
-	elif method == 'MEGASTITCH-SIM-BNDL-ADJ':
-		Sim_GCPs = load_json_sim_gcps(sim_GCPs_path,dataset_name)
-		coords_dict,H,H_inv,abs_tr,res,all_GCPs = field.geo_correct_MegaStitch_Similarity_Bundle_Adjustment_Homography(anchors_dict,Sim_GCPs)
-	elif method == 'MEGASTITCH-AFF-BNDL-ADJ-OLDIN':
-		if dataset_name != "ODFN":
-			Sim_GCPs = load_json_sim_gcps(sim_GCPs_path,dataset_name)
-		else:
-			Sim_GCPs=None
-		coords_dict,H,H_inv,abs_tr,res,all_GCPs = field.geo_correct_MegaStitch_Affine_Bundle_Adjustment_Homography(anchors_dict,Sim_GCPs,use_old_inliers=True)
-	elif method == 'MEGASTITCH-SIM-BNDL-ADJ-OLDIN':
-		Sim_GCPs = load_json_sim_gcps(sim_GCPs_path,dataset_name)
-		coords_dict,H,H_inv,abs_tr,res,all_GCPs = field.geo_correct_MegaStitch_Similarity_Bundle_Adjustment_Homography(anchors_dict,Sim_GCPs,use_old_inliers=True)
-	
-	save_json_results(json_path,res,experiment_Id)
-
-	if H is None:
-		gcp_inf = None
-	else:
-		gcp_inf = (anchors_dict,H_inv,abs_tr)
-		
-	field.generate_transformation_accuracy_histogram(coords_dict,plot_path.replace('initial_GPS','transformation_plot'))
-	field.save_field_centers_visualization(plot_path)
-
-	ortho = field.generate_field_ortho(coords_dict,gcp_info=gcp_inf)
-	
-	field.save_field_ortho(ortho,ortho_path)
-	field.save_field_coordinates(corrected_coordinates_path,coords_dict)
-
-
-	end_time = datetime.datetime.now()
-
-	report_time(start_time,end_time)
-
-	sys.stdout = original
-	log_file.close()
-
-
-	
-
-
+    sys.stdout = original
+    log_file.close()
 
 
 main()
--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # MegaStitch: Robust Large Scale Image Stitching
 
-MegaStitch is a novel method for stitchig high resolution image in large scale while being robust to drift and inconsistencies. It was developed for the Phytooracle team to geo-correct and stitch their high resolution large scale imagesets. You can find the paper [here](https://www.researchgate.net/profile/Ariyan-Zarei/publication/354153722_MegaStitch_Robust_Large_Scale_Image_Stitching/links/612803a70360302a005f3d62/MegaStitch-Robust-Large-Scale-Image-Stitching.pdf). This repository contains all the necessary code to reproduce our results. If you find this method and the paper interesting and use it in your research, please cite us using the following bibliography. 
+MegaStitch is a novel method for stitchig high resolution images in large scale while being robust to drift and inconsistencies. This method was originally developed as an open source tool for the Phytooracle team to geo-correct and stitch their high resolution large scale imagesets. You can find the paper [here](https://www.researchgate.net/profile/Ariyan-Zarei/publication/354153722_MegaStitch_Robust_Large_Scale_Image_Stitching/links/612803a70360302a005f3d62/MegaStitch-Robust-Large-Scale-Image-Stitching.pdf). 
+
+This repository contains all the necessary code to run geo-correction and stitching procedures of MegaStitch method and to reproduce our results. If you find this method and the paper interesting and useful for your research, please cite us using the following bibliography. 
 
 ```
 @article{zarei2021megastitch,
@@ -12,4 +14,29 @@ MegaStitch is a novel method for stitchig high resolution image in large scale w
 
 ```
 
-You can also find all the datasets as well as the Ground Control Points (GCPs) for each of them at [this](https://data.cyverse.org/dav-anon/iplant/projects/phytooracle/papers/MegaStitch/megastitch_data.tar) link.
+This README contains instructions on how to get the data that were used in the paper, install dependencies, and run MegaStitch. 
+
+## Data
+<hr>
+
+You can also find all the datasets as well as the Ground Control Points (GCPs) for each of them at [this](https://data.cyverse.org/dav-anon/iplant/projects/phytooracle/papers/MegaStitch/megastitch_data.tar) link. The tar file contains XXX.
+
+## Requirements and Installation
+<hr>
+
+Currently, there are no installation scripts for this repo. In order to use MegaStitch, you need to make sure that you have all the required python packages and files, and based on your needs, you need to run one of the main entry points of the repo. For your conveniece, we have created a conda requirement file at THIS LINK XXX, from which a conda virtual env can be created using the following command:
+
+```
+conda env create -f requirements.yaml
+```
+
+## Running MegaStitch
+<hr>
+
+MegaStitch can be used to stitch and geo-correct a different variety of images. These images can either contain approximate geo-referencing, or not. We tested MegaStitch on different drone datasets and a set of specific super high resolution images of plants acquized by a moving gantry machine over an agriculture field. 
+
+As one can read in the paper, MegaStitch have different varieties and can be run with different configurations. The main entry point script to be used for stitching and geo-correction drone datasets is `Drone_GPS_Correction.py`. The command to run the code is:
+
+```
+python Drone_GPS_Correction.py 
+```
